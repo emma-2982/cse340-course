dfb6dc1a0602e4a2851bb7d51d6f1395b2915298
emma-2982 <eteye@byupathway.edu>
2026-09-13 15:00:36 +0400

    fix: Update SSL configuration to use rejectUnauthorized false for PostgreSQL connection

--- src/models/db.js
+++ src/models/db.js
@@ -14,7 +14,9 @@ import { Pool } from 'pg';
  */
 const pool = new Pool({
     connectionString: process.env.DB_URL,
-    ssl: true
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 /**
@@ -25,9 +27,7 @@ const pool = new Pool({
  * correct but still see SSL errors, try updating the 'ssl' property in the Pool
  * configuration above to:
  *
- * ssl: {
- *     rejectUnauthorized: false
- * }
+ * ssl: 
  */
 
 /**
